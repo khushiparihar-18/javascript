10fa3ae52d0b03af8c784ab719259a72aad4a4fe
khushiparihar_18 <158739510+khushiparihar-18@users.noreply.github.com>
2025-05-28 10:16:02 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,25 @@
+const name = "khushi"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('khushi-kp')
+
+// console.log(gameName[0])
+// console.log(gameName[1])
+// console.log(gameName[3])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase())
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('p'));
+
+const newString = gameName.substring(0,6)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,3);
+console.log(anotherString)
