e4794ca66aa68e5bca024957b0bd8eb74e8072bf
khushiparihar_18 <158739510+khushiparihar-18@users.noreply.github.com>
2025-05-28 06:03:31 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score = "33kp"
+let sc = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// console.log(typeof sc);
+// console.log(typeof (sc));
+
+// let valueInNumber = Number(score)
+let valInNumber = Number(sc)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// console.log(typeof valInNumber);
+// console.log(valInNumber);
+
+
+let un = undefined
+// console.log(typeof un);
+let v = Number(un);
+// console.log(typeof v)
+// console.log(v);
+
+let isLoggedIn = "khushi"
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => True; 0 => False
+// "" => False
+// "khushi" => True
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
