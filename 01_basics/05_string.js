f3ff1c078bb72f9e9e0883a351aac30e85cb2a2b
Khushi Parihar <khushi18p2005@gmail.com>
2025-05-28 18:03:28 +0530

    string-prototypes

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -15,11 +15,47 @@ const gameName = new String('khushi-kp')
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase())
 
-console.log(gameName.charAt(3));
-console.log(gameName.indexOf('p'));
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('p'));
 
 const newString = gameName.substring(0,6)
-console.log(newString);
+// console.log(newString);
 
 const anotherString = gameName.slice(-8,3);
-console.log(anotherString)
+// console.log(anotherString)
+
+// console.log(name.substring(0,3));
+
+const string1 = gameName.split('-');
+// console.log(string1);
+
+const string2 = gameName.split('k');
+// console.log(string2);
+
+const string3 = gameName.at(5);
+// console.log(string3);
+
+const string4 = gameName.charAt(5);
+// console.log(string4);
+
+let v = (string3 === string4)
+// console.log(v);
+
+const string5 =  gameName.charCodeAt(3);
+// console.log(string5);
+
+const string6 = gameName.includes('kp');
+// console.log(string6);
+
+const string7 = gameName.startsWith('khushi');
+console.log(string7);
+
+const string8 = gameName.endsWith('kp');
+console.log(string8);
+
+const string9 = gameName.replace('khushi', 'khushi-kp');    
+console.log(string9);
+
+console.log(gameName);
+
+
