8389951ec057083a284bd60125206b5c00e4618a
khushiparihar_18 <158739510+khushiparihar-18@users.noreply.github.com>
2025-05-28 06:55:18 +0000

    comparison of Datatypes

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,35 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+// console.log("khushi"<="parihar")
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null>=0);
+// console.log(null<=0);
+// console.log(null==0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+/*
+In JS Comparision and Equality Check Works in Different Fashion!
+
+Comparision Converts into Number then Checks And Equality-Check checks Whether LHS is Equal to RHS or not
+and Triple-Check
+checks for Data-type too !
+*/
+
+console.log("2" == 2);
+console.log("2" === 2);
