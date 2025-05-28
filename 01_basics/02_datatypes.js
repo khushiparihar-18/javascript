e8eba7f9daee1e9ee3796fc05dc1bc511cd4294c
khushiparihar_18 <158739510+khushiparihar-18@users.noreply.github.com>
2025-05-28 05:36:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser, so this will give error -> that : alert is no defined
+
+console.log(3
+    +3)
+// code readability should be high
+console.log("khushi")
+
+let name = "khushi"
+let age = 19
+let isLoggedIn = true //false
+let state 
+let temperature = null
+
+/*
+PRIMITIVE DATA TYPES
+Number => 2^52
+bigint 
+string => "" , ' '
+boolean => true/false
+null => standalone value
+undefined => value not Assigned
+symbol => unique
+
+*/
+
+console.log(typeof "khushi");
+console.log(typeof age);
+console.log(typeof null);     // object
+console.log(typeof undefined);//undefined
+
+
+
