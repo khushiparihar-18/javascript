a7d8d95c01749ae5cae7d72d0f518c535d1dfe6d
khushiparihar_18 <158739510+khushiparihar-18@users.noreply.github.com>
2025-05-28 05:36:02 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "khushi@google.com"
+var accountPassword = "12345"
+accountCity = "Shahpura"
+let accountState ;
+
+// accountId = 2  // it is not allowed
+
+accountEmail = "kp@hacker.com"
+accountPassword = "1820"
+accountCity = "Thana"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
