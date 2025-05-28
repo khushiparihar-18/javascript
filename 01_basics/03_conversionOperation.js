a06e5ad87c14822cd8052fbf4f37083891d7ccef
khushiparihar_18 <158739510+khushiparihar-18@users.noreply.github.com>
2025-05-28 06:20:27 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -35,5 +35,52 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+/* OPERATIONS */
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " khushi"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+8)
+// console.log(1+"8")
+// console.log("1"+"8")
+// console.log("1"+8+" "+2+0)
+// console.log(1+8+"2"+0)
+// console.log(1+8+"2"+0+1+8+2+0)
+// console.log("1"+8+2+0+1+8+2+0)
+
+// console.log(true);
+// console.log(+true);
+// console.log(-true);
+// console.log(+"");
+// console.log(true+) // Syntax Error
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+console.log(++gameCounter);
+console.log(gameCounter++);
+console.log(gameCounter);
+
+
