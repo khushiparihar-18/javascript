5fb5b53ec3e0eddaac379677460b6f34f3118710
Khushi Parihar <khushi18p2005@gmail.com>
2025-05-29 16:45:40 +0530

    dates in JS

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,24 @@
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toDateString());
+console.log(myDate.toUTCString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.getFullYear());
+console.log(myDate.getMonth()); 
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+console.log(myDate.getHours());
+console.log(myDate.getMinutes());
+console.log(myDate.getSeconds());
+console.log(myDate.getMilliseconds());
+console.log(myDate.getTime());
+console.log(myDate.getTimezoneOffset());
+console.log(myDate.getUTCFullYear());       
+console.log(myDate.getUTCMonth());
+console.log(myDate.getUTCDate());
+
