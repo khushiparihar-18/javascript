f26eab5983ab12388a34a9493eb33f68c9153c96
Khushi Parihar <khushi18p2005@gmail.com>
2025-06-03 15:15:08 +0530

    control flow

---
+++ 04_control_flow/01_ifElse.js
@@ -0,0 +1,52 @@
+// if
+const isUserLoggedIn = true;
+const temperature = 41
+
+// if(temperature === 40){
+//     console.log("less than 50");
+// }  else{
+//     console.log("Temperature is greater than 50");
+// }
+// console.log("Execute");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//    let power = "fly"
+//     console.log(`User Power ${power}`);
+    
+// }
+// console.log(`User Power ${power}`);
+
+
+const balance = 1000;
+
+// if(balance > 500) console.log("test"),console.log("test2"); // Never do Like This, This is not a good practice !
+
+// if (balance < 500) {
+//     console.log("Less than 500");
+// } else if (balance < 750) {
+//     console.log("Less than 750");
+// } else if (balance < 900){
+//     console.log("Less than 900");
+// } else {
+//     console.log("Less than 1200");
+// }
+
+
+
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if(userLoggedIn && debitCard && 2 == 3){
+    console.log("Allow to buy course");
+}
+
+if( loggedInFromGoogle || loggedInFromEmail ){
+    console.log("User Logged In");
+}
---
+++ 04_control_flow/01_truthy.js
@@ -0,0 +1,75 @@
+const userEmail = "kp@khushi.ai"
+const userName = ""
+
+if(userEmail){
+    // console.log("Got User Email");
+} else {
+    console.log("Don't have User Email");
+}
+
+if(userName){
+    console.log("Got User Name");
+} else {
+    // console.log("Don't have User Name");
+}
+
+
+/* 
+
+FALSY VALUES
+
+ False,
+ 0,
+ -0,
+BigInt 0n,
+"" (Empty String),
+null,
+undefined,
+NaN (Not a Number);
+
+TRUTHY VALUES
+
+Truthy values are all values that are not falsy.
+
+"0",
+"false",
+" " ( Space String ),
+[] (Empty Array),
+{} (Empty Object),
+function(){} (Empty function),
+
+
+*/
+
+const emptyArray = [];
+
+if (emptyArray.length === 0) {
+    // console.log("Array is Empty");
+}
+
+const emptyObject = {};
+if (Object.keys(emptyObject).length === 0) {
+    // console.log("Object is Empty");
+}
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+console.log(val1);
+
+// Ternary Operator & NCO Both are totally Different
+
+// condition ? true : false 
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("More Than 80") : console.log("Less than 80");
+
+const iceCreamePrice = 70;
+iceCreamePrice <= 70 ? console.log("Less Than 70") : console.log("More than 70");
+
