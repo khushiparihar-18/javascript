f6b5923eb896d34b98d823ca7578f261c2d85322
Khushi Parihar <khushi18p2005@gmail.com>
2025-06-05 15:45:13 +0530

    dom

--- 05_iterations/07_map.js
+++ 05_iterations/07_map.js
@@ -1,6 +1,22 @@
 const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // const newNumbers = myNumbers.map( (num)=> num+10 )
-const newNumbers = myNumbers.map( (num)=> {return num+10} )
-console.log(newNumbers);
+// const newNumbers = myNumbers.map( (num)=> {return num+10} )
+// console.log(newNumbers);
+
+// const n = [];
+// const newNums = myNumbers.forEach( (num)=> { n.push( num+10)})
+// console.log(n);
+
+const newNums = myNumbers
+                    .map( (num) => num * 10 )
+                    .map( (num) => num + 1 )
+                    .filter( (num)=> num >= 40 )
+console.log(newNums);
+
+// const testNums = myNumbers
+//                         .map( (test)=> test > 5 )
+// console.log(testNums);
+
+
 
